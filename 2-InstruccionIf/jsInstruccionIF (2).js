8260e85cccb9c499ef1897e6d9798a2e8c976f00
rsandes90 <40808652+rsandes90@users.noreply.github.com>
2018-07-04 22:13:48 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad;
 
+    edad = document.getElementById("edad").value;
+
+    if(edad >= 18){
+        alert("Es mayor de edad");
+    }
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -2,5 +2,12 @@ function mostrar()
 {
 //tomo la edad  
 
+    var edad;
+    edad = document.getElementById("edad").value;
+
+    if (edad < 13 || edad > 17)  {
+
+        alert("Es adolescente");
+    }
 
 }//FIN DE LA FUNCIÓN
