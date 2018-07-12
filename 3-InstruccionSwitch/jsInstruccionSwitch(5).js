8d5a8292da5a9468e9993a4d926ab4435bcb2f70
rsandes90 <40808652+rsandes90@users.noreply.github.com>
2018-07-12 11:42:01 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,12 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
+var laHora = parseInt(document.getElementById('hora').value); 
+
+switch(laHora) {
+
+    case 7:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+        alert("Es de mañana");
+        
+}
 
 //alert (laHora);
-	
-	
-
-
 
 }//FIN DE LA FUNCIÓN
