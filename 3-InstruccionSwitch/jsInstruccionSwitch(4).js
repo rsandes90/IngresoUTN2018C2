77c7b82edc9e896ffa80604667348be659b422d9
rsandes90 <40808652+rsandes90@users.noreply.github.com>
2018-07-11 22:15:43 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,19 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+switch(mesDelAño) {
+    case ("Febrero"):
+        alert("Tiene 28 dias");
+        break;
+    case("Abril"):
+    case("Julio"):
+    case("Septiembre"):
+    case("Noviembre"):
+        alert("Tiene 30 dias");
+        break;
+    default:
+        alert("Tiene 31 dias");        
+}	
 	
 
 
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+    var numeroUno;
+    var numeroDos;
+    var resultado;
 
+    numeroUno = prompt("Ingresa el primer numero: ");
+    numeroDos = prompt("Ingresa el segundo numero: ");
+
+    if(parseInt(numeroUno) == parseInt(numeroDos)) {
+        alert(numeroUno+numeroDos);
+    } else if (parseInt(numeroUno) > parseInt(numeroDos)){
+        resultado = numeroUno - numeroDos;
+        alert("Resultado: "+resultado);        
+    } else {
+        resultado = parseInt(numeroUno) + parseInt(numeroDos);
+        alert("Resultado: "+resultado);
+
+        if (resultado > 10){
+            alert("La suma es: "+resultado+" y supero el 10");
+        }    
+    }
 }
