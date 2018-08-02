71a672a48255d7961fb6911a302a595d2549c4cc
rsandes90 <40808652+rsandes90@users.noreply.github.com>
2018-08-01 21:49:42 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,58 @@
 function mostrar()
 {
+    var respuesta;    
+    var contPares = 0;
+    var contImpares = 0;
+    var contCeros = 0;
+    var contPositivos = 0;
+    var acumPositivos = 0;
+    var acumNegativos = 0;
+    var numMax;
+    var letraMax;
+    var numMin;
+    var letraMin;
+    var flag = true;
+    
+    do {
+
+        var letra = prompt("Ingrese una letra: ");
+        var numero = parseInt( prompt("Ingrese un numero: "));
+
+        while (numero < -100 || numero > 100 || isNaN(numero)) {
+            numero = prompt("Numero incorrecto. Reingrese un numero: ");
+        }
+
+        if (numero % 2 = 0) {
+            contPares ++;
+        } else {
+            contImpares ++;
+        }
+
+        if (numero == 0) {
+            contCeros ++;
+        } else if (numero > 0) {
+            acumPositivos = acumPositivos + numero;
+            contPositivos ++;
+        } else {
+            acumNegativos = acumNegativos + numero;
+        }
+
+        if (numero > numMax || flag == true) {
+            numMax = numero;
+            letraMax = letra;
+
+        }
+
+        if (numero < numMin || flag2 == true) {
+            numMin = numero;
+            letraMin = letra;
+
+            flag2 = false;
+        }
+
+        respuesta = prompt("Quiere continuar?: ");
+
+    } while(respuesta != 'si');
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,45 @@
 function mostrar()
 {
+    var acumNotas = 0;
+    var promedioNotas = 0;
+    var notaBaja; 
+    var sexoBajo;
+    var varonesAprobados = 0;
+    var flag = true;
+
+    for ( i = 0; i < 5; i ++){
+
+    var nota = parseInt( prompt("Ingrese una nota: ") );
+    var sexo = prompt("Ingrese un sexo: ");    
+
+    while (nota < 0 || nota > 10){
+        nota = prompt("Nota incorrecta. Ingrese la nota nuevamente: ");
+    }
+
+    while (sexo != 'm' && sexo != 'f'){
+        sexo = prompt("Sexo incorrecto. Ingrese el sexo nuevamente: ");
+    }
+
+    acumNotas = acumNotas + nota;
+
+    if (nota < notaBaja || flag == true) {
+        notaBaja = nota;
+        sexoBajo = sexo;
+
+        flag = false;
+    }
+
+    if (nota >= 6 && sexo == 'm'){
+        varonesAprobados ++;
+    }
+
+ }
+
+ promedioNotas = acumNotas / 5;
+
+ alert(" Promedio de Notas: " + promedioNotas + 
+       "\n Nota mas baja: " + notaBaja + 
+       "\n Sexo mas bajo: " + sexoBajo + 
+       "\n Cantidad de varones aprobados: " + varonesAprobados);
 
 }
